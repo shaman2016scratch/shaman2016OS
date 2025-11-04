87acde79555f912b7ba244111e9dcfde4c1a395a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-04 11:38:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 docData = {}
 docData.ecran = document.getElementById('ecran')
 docData.ecran2 = document.getElementById('ecran2')
+async function zagruz() {
+  wait(1200)
+  docData.ecran.innerHTML = '<text x="50%" y="50%" font-size="36" fill="black">Loading OS.</text>'
+  wait(1200)
+  docData.ecran.innerHTML = '<text x="50%" y="50%" font-size="36" fill="black">Loading OS..</text>'
+  wait(1200)
+  docData.ecran.innerHTML = '<text x="50%" y="50%" font-size="36" fill="black">Loading OS...</text>'
+  wait(1200)
+  docData.ecran.innerHTML = '<text x="50%" y="50%" font-size="36" fill="black">Loading OS</text>'
+}
+for (let i = 0; i < 5; i++) {
+  zagruz()
+}
